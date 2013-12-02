85919e6a1f0f80317e645109d916333246e82db0
edwardlando <edwardlando8@gmail.com>
2013-12-02 00:09:58 -0500

    static

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamocracyStatic::Application.config.secret_token = 'fb579e023d21d3e6615e6618b7be33a6b2abdf648b40bf76b9bc6016d5ff6f8ff9988615313b318d68f500adaf037472724776a9afcfc7cee1ebbb70e859f5dc'
